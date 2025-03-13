318833f2eebadff583ffe3c2919273b51b040fa9
github-action-benchmark <github@users.noreply.github.com>
2025-03-13 10:01:11 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 69858d1615e1775bf7f1256027c4e720870b9bbb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741859930302,
+  "lastUpdate": 1741860071519,
   "repoUrl": "https://github.com/moonbit-community/cmark",
   "entries": {
     "Benchmark.js Benchmark": [
@@ -32,6 +32,40 @@ window.BENCHMARK_DATA = {
             "name": "cmarkJS(spec * 200)",
             "value": 0.2,
             "range": "±5.27%",
+            "unit": "ops/sec",
+            "extra": "5 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "moonbit-community",
+            "username": "moonbit-community"
+          },
+          "committer": {
+            "name": "moonbit-community",
+            "username": "moonbit-community"
+          },
+          "id": "69858d1615e1775bf7f1256027c4e720870b9bbb",
+          "message": "bench: add benchmark suite",
+          "timestamp": "2025-03-12T09:43:01Z",
+          "url": "https://github.com/moonbit-community/cmark/pull/51/commits/69858d1615e1775bf7f1256027c4e720870b9bbb"
+        },
+        "date": 1741860070637,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmarkJS(spec * 100)",
+            "value": 0.44,
+            "range": "±6.29%",
+            "unit": "ops/sec",
+            "extra": "6 samples"
+          },
+          {
+            "name": "cmarkJS(spec * 200)",
+            "value": 0.21,
+            "range": "±5.18%",
             "unit": "ops/sec",
             "extra": "5 samples"
           }
