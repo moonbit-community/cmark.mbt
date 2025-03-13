4aa5ef6ba425cf414c49dcbcf5cfd613629fd629
github-action-benchmark <github@users.noreply.github.com>
2025-03-13 10:08:48 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for f18851be35ef0f30ebd1854167e6dabbc8bcb543

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741860071519,
+  "lastUpdate": 1741860528478,
   "repoUrl": "https://github.com/moonbit-community/cmark",
   "entries": {
     "Benchmark.js Benchmark": [
@@ -66,6 +66,44 @@ window.BENCHMARK_DATA = {
             "name": "cmarkJS(spec * 200)",
             "value": 0.21,
             "range": "±5.18%",
+            "unit": "ops/sec",
+            "extra": "5 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rami3l@outlook.com",
+            "name": "rami3l",
+            "username": "rami3l"
+          },
+          "committer": {
+            "email": "rami3l@outlook.com",
+            "name": "rami3l",
+            "username": "rami3l"
+          },
+          "distinct": true,
+          "id": "f18851be35ef0f30ebd1854167e6dabbc8bcb543",
+          "message": "ci: add `bench`",
+          "timestamp": "2025-03-13T18:06:28+08:00",
+          "tree_id": "7a714d1b4fd55d66889374df1f318baec9e635c7",
+          "url": "https://github.com/moonbit-community/cmark/commit/f18851be35ef0f30ebd1854167e6dabbc8bcb543"
+        },
+        "date": 1741860528174,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmarkJS(spec * 100)",
+            "value": 0.41,
+            "range": "±8.33%",
+            "unit": "ops/sec",
+            "extra": "6 samples"
+          },
+          {
+            "name": "cmarkJS(spec * 200)",
+            "value": 0.2,
+            "range": "±3.35%",
             "unit": "ops/sec",
             "extra": "5 samples"
           }
